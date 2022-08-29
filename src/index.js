b442e348e561024d5d70c9d7f856f7c14d03b363
yrpcfcp <67036488+yrpcfcp@users.noreply.github.com>
2022-08-29 12:22:54 -0300

    Finished project

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import NameTag from "./NameTag"
-
+import App from "./App";
 import "./styles.css";
-
-const App = () => (
-  <div className="App">
-    <h1>Name Tag Generator</h1>
-    <NameTag name="Carlos"/>
-    <NameTag name="Ana"/>
-    <NameTag name="Shirley"/>
-    <NameTag name="Maria"/>
-    <NameTag name="Thomas"/>
-  </div>
-);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
